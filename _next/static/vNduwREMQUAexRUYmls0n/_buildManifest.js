49a4dd413af84eca3c146b0e8f10925cb95bbdb0
Lei Jiachen <leijiachen@LeideMacBook-Pro.local>
2024-09-14 09:50:50 +0800

    update

---
+++ _next/static/vNduwREMQUAexRUYmls0n/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/333-3e66d92038cdaa8d.js","static/css/89398dd79d9143ca.css","static/chunks/pages/index-6c76019c6e85dc9b.js"],"/_error":["static/chunks/pages/_error-1be831200e60c5c0.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
